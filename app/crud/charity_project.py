76d53b5878512e1aa8a130adc2f34b40b218e89e
Сулайманова Сайкал <sulaymanovasaykal2003gmail.com>
2024-04-28 01:00:07 +0300

    401

--- app/crud/charity_project.py
+++ app/crud/charity_project.py
@@ -1,12 +1,28 @@
+from sqlalchemy import Date, TIMESTAMP, extract
+
 from app.crud.base import CRUDBase
 from app.models import CharityProject
 from app.schemas.charity_project import (CharityProjectCreate,
                                          CharityProjectUpdate)
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import and_, between, func, or_, select
 
 
 class CRUDCharityproject(
         CRUDBase[CharityProject, CharityProjectCreate, CharityProjectUpdate]):
-    pass
 
+    async def get_projects_by_completion_rate(
+            self,
+            session: AsyncSession,
+    ) -> list[dict[str, str]]:
+        project = await session.execute(
+            select(
+                CharityProject.name,
+                (func.extract('epoch', CharityProject.close_date) -
+                func.extract('epoch', CharityProject.create_date)).label('time_diff'),
+                CharityProject.description
+            ).where(CharityProject.fully_invested == True).order_by('time_diff')
+        )
+        return project.all()
 
 charity_project_crud = CRUDCharityproject(CharityProject)
--- app/main.py
+++ app/main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
 
 from app.api.routers import main_router
+from app.core import create_first_superuser
 
 app = FastAPI()
 app.include_router(main_router)
+
+
+@app.on_event('startup')
+async def startup():
+    await create_first_superuser()
